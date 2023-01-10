7f75ac775f8df43fe49262310cbac15be733ab55
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-01-10 15:18:13 -0500

    Updated

--- src/components/Inicio/Procesos.tsx
+++ src/components/Inicio/Procesos.tsx
@@ -10,22 +10,73 @@ const Procesos: React.FC = () => {
                 </div>
                 <div id='pasos_proceso'>
                     <div id='pasos_proceso_icons'>
-                    <div id='pasos_proceso_icons_superior'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" id='icon_down'/>
-                    </svg>
-                    </div>
-                    <div id='pasos_proceso_icons_inferior'>
-
-                    </div>
+                        <div id='pasos_proceso_icons_superior'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" id='icon_down'/>
+                        </svg>
+                        </div>
+                        <div id='pasos_proceso_icons_inferior'>
+                        </div>
                     </div>
                     <div id='pasos_proceso_palabras'>
-                    <div id='pasos_proceso_palabras_superior'>
-
+                        <div id='pasos_proceso_palabras_superior'>
+                            Paso 1    
+                        </div>
+                        <div id='pasos_proceso_palabras_inferior'>
+                            Analiza y escoge la opción más conveniente a su necesidad en cuestión del valor y los meses en los que quieres realizar el pago.
+                        </div>
                     </div>
-                    <div id='pasos_proceso_palabras_inferior'>
-
+                </div>
+                <div id='pasos_proceso'>
+                    <div id='pasos_proceso_icons'>
+                        <div id='pasos_proceso_icons_superior'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" id='icon_down'/>
+                        </svg>
+                        </div>
+                        <div id='pasos_proceso_icons_inferior'>
+                        </div>
                     </div>
+                    <div id='pasos_proceso_palabras'>
+                        <div id='pasos_proceso_palabras_superior'>
+                            Paso 2  
+                        </div>
+                        <div id='pasos_proceso_palabras_inferior'>
+                            Llenas el formulario de nueva solicitud según los términos y cóndiciones.
+                        </div>
+                    </div>
+                </div>
+                <div id='pasos_proceso'>
+                    <div id='pasos_proceso_icons'>
+                        <div id='pasos_proceso_icons_superior'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" id='icon_down'/>
+                        </svg>
+                        </div>
+                        <div id='pasos_proceso_icons_inferior'>
+                        </div>
+                    </div>
+                    <div id='pasos_proceso_palabras'>
+                        <div id='pasos_proceso_palabras_superior'>
+                            Paso 3
+                        </div>
+                        <div id='pasos_proceso_palabras_inferior'>
+                            Esperas la validación de nuestra entidad, te damos la respuesta positiva o negativa a la solicitud.
+                        </div>
+                    </div>
+                </div>
+                <div id='pasos_proceso'>
+                    <div id='pasos_proceso_icons_aprobado'>
+                        <div id='pasos_proceso_icons_superior'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" id='icon_down'/>
+                        </svg>
+                        </div>
+                    </div>
+                    <div id='pasos_proceso_palabras'>
+                        <div id='pasos_proceso_palabras_superior'>
+                        ¡Listo!. te desembolsamos 💵
+                        </div>
                     </div>
                 </div>
                 </div>
