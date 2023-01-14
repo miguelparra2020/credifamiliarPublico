ea32eb3b554c693f258c3e47e1357b94a5b1bcec
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-01-14 17:06:15 -0500

    Updated

--- src/components/solicitar/OpcionesCreditos.tsx
+++ src/components/solicitar/OpcionesCreditos.tsx
@@ -1,11 +1,34 @@
 import './OpcionesCreditos.css';
 // import { IonIcon} from '@ionic/react';
 // import {  flagOutline } from 'ionicons/icons';
+import React from 'react';
+import {  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel  } from '@ionic/react';
 const OpcionesCreditos: React.FC = () => {
     return (
         <div id='contenedor'>
-            hola
+            <IonCard>
+                <IonCardHeader>
+                    <IonCardTitle>Escoge la mejor opción Acorde a su necesidad</IonCardTitle>
+                    <IonCardSubtitle>Te podemos ofrecer: </IonCardSubtitle>
+                </IonCardHeader>
+
+                <IonCardContent>
+                    <IonCardSubtitle>Escoge la cantidad de dinero</IonCardSubtitle>
+                    <IonItem href="#">
+                        <IonLabel>$100.000 "Cien mil pesos m/cte" 💵</IonLabel>
+                    </IonItem>
+                    <IonItem href="#">
+                        <IonLabel>$200.000 "Docientos mil pesos m/cte" 💵</IonLabel>
+                    </IonItem>
+                    <IonItem href="#">
+                        <IonLabel>$300.000 "Trescientos mil pesos m/cte" 💵</IonLabel>
+                    </IonItem>
+                </IonCardContent>
+            </IonCard>
         </div>
     );
 };
 export default OpcionesCreditos;
+
+
+
