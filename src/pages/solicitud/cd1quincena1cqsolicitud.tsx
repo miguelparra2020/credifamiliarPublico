ad57d4f289e829a89ad854a88521a267160bf123
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-01-28 00:07:30 -0500

    Updated

--- src/pages/solicitud/cd1quincena1cqsolicitud.tsx
+++ src/pages/solicitud/cd1quincena1cqsolicitud.tsx
@@ -46,13 +46,13 @@ setTimeout(() => {
         
         <div id="contenido_body">
             <div id='contenido_div2'>
-            <IonButton href='/solicitud/cd1mes1cm'>
+            <IonButton href='/solicitud/cd1quincena1cq'>
             <IonChip color="success">
                 <IonIcon icon={arrowUndoCircleOutline}></IonIcon>
                 <IonLabel>Volver</IonLabel>
             </IonChip>
             </IonButton>
-            <Iframe url="https://script.google.com/macros/s/AKfycbyDPply7O6IGY6zCfnhiLkouyP9yGdNJvd8RrZ2aNx_n9w-BD5ntyHYLTGqKyakjtwW/exec"
+            <Iframe url="https://script.google.com/macros/s/AKfycbyqcrSRgRSAEYn7-USLh3xjRO9TJ3_I4frWNiYrx57QQhkzsgOwdMS1WS7S2IuIwWy1Ag/exec"
                 width="350px"
                 height="3000px"
                 id=""
