faa883863f7fd5dc681c9e272a958f1897e50d17
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-01-29 16:42:39 -0500

    Updated

--- src/pages/solicitud/cd2mes3cmsolicitud.tsx
+++ src/pages/solicitud/cd2mes3cmsolicitud.tsx
@@ -46,13 +46,13 @@ setTimeout(() => {
         
         <div id="contenido_body">
             <div id='contenido_div2'>
-            <IonButton href='/solicitud/cd1mes1cm'>
+            <IonButton href='/solicitud/cd2mes3cm'>
             <IonChip color="success">
                 <IonIcon icon={arrowUndoCircleOutline}></IonIcon>
                 <IonLabel>Volver</IonLabel>
             </IonChip>
             </IonButton>
-            <Iframe url="https://script.google.com/macros/s/AKfycbyDPply7O6IGY6zCfnhiLkouyP9yGdNJvd8RrZ2aNx_n9w-BD5ntyHYLTGqKyakjtwW/exec"
+            <Iframe url="https://script.google.com/macros/s/AKfycbxf_BPOHgz6-wA8caFckU68uLcXdVnWXOd5VwimdAgOzHVEtuXKSj5Nwk9VRBl0qwE7Uw/exec"
                 width="350px"
                 height="3000px"
                 id=""
